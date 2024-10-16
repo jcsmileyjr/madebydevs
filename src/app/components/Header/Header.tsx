7c89cd378a2bfe93858e959075ef408516136d5a
JC <jcsmileyjr@gmail.com>
2024-10-16 16:18:58 -0500

    Feat - Initial Header component setup and outline

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+
+export default function Header() {
+    return (
+        <div className="bg-amber-300 w-full p-4 flex flex-row justify-between">
+            <p>Made by Devs</p>
+            <button type="button">Visit Site</button>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-import Image from 'next/image'
+//import Image from 'next/image'
+import Header from "./components/Header/Header"
 
+/**
+ *    Header
+ *    - Site Logo 
+      - VisitSiteButton component
+      Content 
+      ToggleContainer component
+      - FileUpload components
+      - InputField component with object propety
+      ToggleContainer component
+      ToggleContainer component
+      SubmitButton component
+ * 
+ */
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Test main page
+    <main className="flex min-h-screen flex-col items-center justify-between bg-cyan-100">
+      <Header />
+
     </main>
   )
 }
