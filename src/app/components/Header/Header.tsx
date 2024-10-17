7060c3838554147b66d95bcc05ddfe07be7f458e
JC <jcsmileyjr@gmail.com>
2024-10-17 18:56:43 -0500

    Style - Update the Header to look like the design

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,9 +1,9 @@
 
 export default function Header() {
     return (
-        <div className="bg-amber-300 w-full p-4 flex flex-row justify-between">
-            <p>Made by Devs</p>
-            <button type="button">Visit Site</button>
+        <div className="w-full p-4 pt-8 flex flex-row justify-between sm:justify-center items-center text-white">
+            <p className="font-bold text-primaryBlue text-4xl">Made by Devs</p>
+            <button className="bg-primaryBlue text-white p-2 rounded-lg sm:block sm:absolute sm:right-20" type="button">Visit Site</button>
         </div>
     )
 }
